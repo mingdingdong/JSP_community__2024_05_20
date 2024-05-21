728ac8422b1c0668b03f7e3c0cb53389d8e6ec1f
mingdingdong <gksals520@duksung.ac.kr>
2024-05-21 14:24:28 +0900

    구구단에 model2 방식 적용

--- src/main/java/com/sbs/exam/servlet/GugudanServlet.java
+++ src/main/java/com/sbs/exam/servlet/GugudanServlet.java
@@ -1,6 +1,7 @@
 package com.sbs.exam.servlet;
 
 import com.sbs.exam.Rq;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -18,9 +19,13 @@ public class GugudanServlet extends HttpServlet {
         int dan = rq.getIntParam("dan",0);
         int limit = rq.getIntParam("limit",0);
 
-        rq.appendBody("<div class=\"a\">%d단</div>".formatted(dan));
-        for (int i = 1; i <= limit; i++) {
-            resp.getWriter().append("<div>%d * %d = %d</div>".formatted(dan, i , dan*i));
-        }
+        // request에 정보를 담는다.
+        // 왜냐하면 나중에 gugudan2.jsp에서 해당 내용을 꺼낼 수 있기 때문에
+        req.setAttribute("dan",dan);
+        req.setAttribute("limit",limit);
+
+        // gugudan2.jsp 에게 나머지 작업을 토스
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/gugudan2.jsp");
+        requestDispatcher.forward(req, resp);
     }
 }
