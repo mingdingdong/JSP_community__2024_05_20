bf0a71588225ed1d5f385c099153aa5eebe3ea06
mingdingdong <gksals520@duksung.ac.kr>
2024-05-21 11:29:47 +0900

    구구단 JSP 도입

--- src/main/java/GugudanServlet.java
+++ src/main/java/GugudanServlet.java
@@ -15,7 +15,7 @@ public class GugudanServlet extends HttpServlet {
         int dan = rq.getIntParam("dan",0);
         int limit = rq.getIntParam("limit",0);
 
-        resp.getWriter().append("<h1>%d단</h1>".formatted(dan));
+        rq.appendBody("<h1>%d단</h1>".formatted(dan));
         for (int i = 1; i <= limit; i++) {
             resp.getWriter().append("<div>%d * %d = %d</div>".formatted(dan, i , dan*i));
         }
